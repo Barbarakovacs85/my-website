afaf75885a6f07a907aad4661a808da745f36623
Barbarakovacs85 <133600947+Barbarakovacs85@users.noreply.github.com>
2026-04-11 21:57:48 +0200

    chat

--- src/components/ChatWidget/ChatWidget.js
+++ src/components/ChatWidget/ChatWidget.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./ChatWidget.css";
 
-import { app } from "./firebase";
+import { app } from "../../firebase";
 import {
   getFirestore,
   collection,
