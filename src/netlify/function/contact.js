fbea522aabb08a8a1fa7245777ec7df70d911343
Barbarakovacs85 <133600947+Barbarakovacs85@users.noreply.github.com>
2026-04-11 20:39:25 +0200

    netlify

---
+++ src/netlify/function/contact.js
@@ -0,0 +1,39 @@
+const nodemailer = require("nodemailer");
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Only POST allowed",
+    };
+  }
+
+  try {
+    const { name, email, message } = JSON.parse(event.body);
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: email,
+      to: process.env.EMAIL_USER,
+      subject: `Új üzenet: ${name}`,
+      text: `Név: ${name}\nEmail: ${email}\nÜzenet: ${message}`,
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
